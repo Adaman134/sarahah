fa4811d0d391c72fd7d76cf1f475811217d836f3
Adaman134 <Adaman134@gmail.com>
2017-09-19 18:16:08 -0400

    added sarahah files

---
+++ js/messages_ar.js
@@ -0,0 +1,23 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: AR (Arabic; العربية)
+ */
+$.extend($.validator.messages, {
+    required: "هذا الحقل إلزامي",
+    remote: "يرجى تصحيح هذا الحقل للمتابعة",
+    email: "يرجى إدخال بريد إلكتروني صحيح",
+    url: "يرجى إدخال رابط موقع إلكتروني صحيح",
+    date: "يرجى إدخال تاريخ صحيح",
+    dateISO: "يرجى إدخال تاريخ صحيح (ISO)",
+    number: "يرجى إدخال عدد بطريقة صحيحة",
+    digits: "يرجى إدخال أرقام فقط",
+    creditcard: "يرجى إدخال رقم بطاقة ائتمان صحيح",
+    equalTo: "يرجى إدخال نفس القيمة",
+    extension: "يرجى إدخال ملف بامتداد موافق عليه",
+    maxlength: $.validator.format("الحد الأقصى لعدد الحروف هو {0}"),
+    minlength: $.validator.format("الحد الأدنى لعدد الحروف هو {0}"),
+    rangelength: $.validator.format("عدد الحروف يجب أن يكون بين {0} و {1}"),
+    range: $.validator.format("يرجى إدخال عدد قيمته بين {0} و {1}"),
+    max: $.validator.format("يرجى إدخال عدد أقل من أو يساوي (0}"),
+    min: $.validator.format("يرجى إدخال عدد أكبر من أو يساوي (0}")
+});
